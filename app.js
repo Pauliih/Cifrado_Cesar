40d31bce822c7023d0d78fcda844bf1f839e3d30
Pauliih <pau.campos89@gmail.com>
2017-10-04 03:01:36 -0300

    html y app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+/*1. Cifrado César
+
+Crea una web que pida, por medio de un prompt(), una frase al usuario y devuelva el mismo mensaje encriptado según el algoritmo de Cifrado César con el parámetro de desplazamiento de 33 espacios hacia la derecha
+
+Por ejemplo:
+
+Texto original: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+Texto codificado: HIJKLMNOPQRSTUVWXYZABCDEFG
+A continuación un video de Michelle que te lleva a través de la fórmula matemática del Cifrado César y un par de cosas más que debes saber para resolver este reto. ¡Escúchala con detenimiento y sigue sus consejos! :)
+#############################################################################*/
