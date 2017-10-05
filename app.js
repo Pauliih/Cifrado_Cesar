1799a5be70dce36ba75afad0eed733f1283257d8
Pauliih <pau.campos89@gmail.com>
2017-10-05 15:51:22 -0300

    Cifra, no descifra

--- app.js
+++ app.js
@@ -35,6 +35,23 @@ Consideraciones Específicas
 	- Que contenga números
 
 	###########################################################################################*/
+function menu(msg){
+	do{
+		msg = prompt("Indique el número de lo que desea hacer: 1)Cifrar - 2)Decifrar");
+		if(msg != ""){
+			if(msg == "1") {
+				cipher();
+			} else if (msg == "2") {
+				decipher();
+			} else {
+				alert("Ingrese una opción válida");
+			}
+		}
+	} while (msg == "" || (msg != "1" && msg != "2"));
+}
+
+menu();
+
 function cipher(msg){
 	do{
 		msg=prompt("Escribe tu mensaje a cifrar:");
@@ -54,13 +71,38 @@ function cipher(msg){
 	
 	for (var i = 0; i < arrayCipher.length; i++) {
 	   	strCipher.push(String.fromCharCode(arrayCipher[i]));
-	   	cesarCiphed=strCipher.join('');
+	   	caesarCiphed=strCipher.join('');
 	}	
-	   alert(cesarCiphed);
+	   return alert(caesarCiphed);
 }
 
-function decipher(){
+/*
+- String.charCodeAt(indice); -> método devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.
+Ejemplo: "ABC".charCodeAt(0) // returns 65
 
+- String.fromCharCode(indice); -> estatico método devuelve una cadena creada mediante 
+el uso de una secuencia de valores Unicode especificada.
+Ejemplo: String.fromCharCode(65,66,67) // return 'ABC';*/
+
+function decipher(msg){
+	do{
+		msg=prompt("Escribe tu mensaje a descifrar:");
+
+	}while(msg== null || msg.length <= 0 || /^\s+$/.test(msg) || !isNaN(msg));
+	// La fórmula para descifrar es: (x - n) % 26	
+		var arrayDecipher=[];
+		var strDecipher= [];
+	
+	for (var i = 0; i < msg.length; i++) {
+	   	strAscii=msg.charCodeAt(i); //Le saco el cod. ASCII a la letra del msg inicial
+
+	   	//Fórmula descifrar || NO FUNCIONA FORMULA!
+	   	arrayDecipher.push((strAscii - 33)%26 ); //Ingreso al arrayDecipher el cod. ASCII de la letra pasad por la fórmula y lo ingreso al arrayDecipher
+	}
+	
+	for (var i = 0; i < arrayDecipher.length; i++) {
+	   	strDecipher.push(String.fromCharCode(arrayDecipher[i]));
+	   	caesarDeciphed=strDecipher.join('');
+	}	
+	   return alert(caesarDeciphed);
 }
-
-cipher();
